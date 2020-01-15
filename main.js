dc7e5db90cb277e9aa581fac383cbc76aa577de1
petarantic <petar.cntic94@gmail.com>
2020-01-15 15:52:03 +0100

    Task completed

--- main.js
+++ main.js
@@ -12,83 +12,76 @@ fetch('https://restcountries-v1.p.rapidapi.com/all', {
     countryNames.push(response[i].name);
   };
   
-  let ul =  document.getElementById('listOfCountries')
+  const countriesList =  document.getElementById('listOfCountries')
   
   for (let i=0; i < countryNames.length; i++){
     let li = document.createElement('li');
     li.innerHTML = countryNames[i];
-    ul.appendChild(li); 
-    clickFunction(li, i);
+    countriesList.appendChild(li); 
+    chooseCountry(li, i);
   };
 
-
-
-function clickFunction (li, i) { 
-  li.onclick = function () {
-    $('.box').addClass('hide');
-    $('.box2').removeClass('hide'); 
-    let fetchURL = 'https://restcountries-v1.p.rapidapi.com/name/';
-    getQuote(fetchURL, response[i].name);
-  }
-}
-  
-
-function getQuote (fetchURL, name) {
-  fetch(fetchURL + name, {
-    'method': 'GET',
-    'headers': {
-      'x-rapidapi-host': 'restcountries-v1.p.rapidapi.com',
-      'x-rapidapi-key': 'c5ce38b518msh74369a3a3425dc6p1b7f01jsn246f0fd6c1bb'
+  function chooseCountry(li, i) { 
+    li.onclick = function () {
+      const fetchURL = 'https://restcountries-v1.p.rapidapi.com/name/';
+      $('.box').addClass('hide');
+      $('.box2').removeClass('hide'); 
+      getQuote(fetchURL, response[i].name);
     }
-  })
-  .then(response => response.json())
-  .then(response => {
-    let found = response.find(function (country, index) { 
-      return country.name === name;
-    });
+  }
     
-    let capital = 'Capital: ' + found.capital;
-    let population = 'Population: ' + found.population;
-    let urlbeg= 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAMUhZJImC93BePVKuTu17GN1En2mXhxS0&q=';
-    let natName = 'Native name: ' + found.nativeName;
-  
-    $('#capital').html(capital);
-    $('#population').html(population);
-    $('#map').attr('src', urlbeg + found.name);
-    $('#nativeName').html(natName);
-  })
-  .catch(err => {           
-    console.log('error');
-  });
-};
+  function getQuote(fetchURL, name) {
+    fetch(fetchURL + name, {
+      'method': 'GET',
+      'headers': {
+        'x-rapidapi-host': 'restcountries-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'c5ce38b518msh74369a3a3425dc6p1b7f01jsn246f0fd6c1bb'
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      const found = response.find(function (country, index) { 
+        return country.name === name;
+      });
+      
+      const capital = 'Capital: ' + found.capital;
+      const population = 'Population: ' + found.population;
+      const urlbeg= 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAMUhZJImC93BePVKuTu17GN1En2mXhxS0&q=';
+      const natName = 'Native name: ' + found.nativeName;
+    
+      $('#capital').html(capital);
+      $('#population').html(population);
+      $('#map').attr('src', urlbeg + found.name);
+      $('#nativeName').html(natName);
+    })
+    .catch(err => {           
+      console.log('error');
+    });
+  };
 
-$('#back').click(function back(){
+  $('#back').click(function back(){
     $('.box').removeClass('hide');
     $('.box2').addClass('hide');
-});
+  });
 
-document.getElementById('countriesSearch').addEventListener('input', myFunction);
+  document.getElementById('countriesSearch').addEventListener('input', search);
 
-function myFunction () {
-  let input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('countriesSearch');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById('listOfCountries');
-  li = ul.getElementsByTagName('li');
-   
-  for (i = 0; i < li.length; i++) {
-    a = li[i];
-    txtValue = a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = '';
-    } else {
-      li[i].style.display = 'none';
+  function search() {
+    const input = document.getElementById('countriesSearch');
+    const filter = input.value.toUpperCase();
+    let li = countriesList.getElementsByTagName('li');
+     
+    for (i = 0; i < li.length; i++) {
+      a = li[i];
+      const = a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = '';
+      } else {
+        li[i].style.display = 'none';
+      }
     }
-  }
-};
+  };
 })
 .catch(err => {
   console.log('err');
 });
-
-
