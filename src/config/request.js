90601dde3152dc6e68cec92eec1542237490558b
petarantic <petar.cntic94@gmail.com>
2020-02-08 00:59:41 +0100

    Implementing webpack

--- src/config/request.js
+++ src/config/request.js
@@ -5,3 +5,5 @@ const request = {
 		'x-rapidapi-key': 'c5ce38b518msh74369a3a3425dc6p1b7f01jsn246f0fd6c1bb'
   }
 };
+
+export default request;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'development',
+  mode: 'development',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
